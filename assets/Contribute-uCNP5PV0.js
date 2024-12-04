441df4546251b485cb99547b31e3165e7d43ce34
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2024-12-04 14:11:16 +0800

    Update Contribute-uCNP5PV0.js

--- assets/Contribute-uCNP5PV0.js
+++ assets/Contribute-uCNP5PV0.js
@@ -12,5 +12,5 @@ import{m as d}from"./github-markdown-BOpQ1pHb.js";import{_,e as i,f as p,c as u,
 3. 在\`题目名称\`文件夹下放入输入、输出文件，并编写problem.json（格式参考已有题目）
 4. 在仓库根目录下运行\`python encrypt.py ./problems/题目名称\`，会生成in.enc和out.enc文件
 5. 删除\`题目名称\`文件夹下的in文件和out文件
-6. \`git add .\` \`git push\`将本地更改应用到fork仓库中
+6. \`git add .\` \`git commit\` \`git push\`将本地更改应用到fork仓库中
 7. 发起PR，等待审核`)}),(v,r)=>{const a=t("el-card"),c=t("el-col"),l=t("el-row");return m(),u("div",f,[o(l,{justify:"center"},{default:e(()=>[o(c,{span:20},{default:e(()=>[o(a,null,{header:e(()=>r[0]||(r[0]=[n("div",{class:"header"},[n("h2",null,"贡献题目")],-1)])),default:e(()=>[n("div",{class:"markdown-body",innerHTML:s.value},null,8,b)]),_:1})]),_:1})]),_:1})])}}},x=_(h,[["__scopeId","data-v-584426d8"]]);export{x as default};
