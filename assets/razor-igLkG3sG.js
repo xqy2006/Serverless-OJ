3332d55cdd5eb8612acf212c98a5614f29dad663
xuqinyang <2601928946@qq.com>
2025-01-01 15:23:34 +0800

    update

--- assets/razor-igLkG3sG.js
+++ assets/razor-igLkG3sG.js
@@ -1,4 +1,4 @@
-import{m}from"./toggleHighContrast-C4MJ7g_y.js";import"./index-B4MNqG0y.js";/*!-----------------------------------------------------------------------------
+import{m}from"./toggleHighContrast-BjiC_fUZ.js";import"./index-q5-IUobq.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
